6fc1e3486bbb939bbda17409c9dd1a4cf60b9a88
Kowalczuk <wkowalczuk03@gmail.com>
2026-03-24 21:03:05 +0100

    usuwanie oraz poprawianie danych dziala

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.post('/msg',urlencodedParser , function(req, res) {
     console.log("Message from client side: " + req.body.title);
     console.log("2nd message from client side: " + req.body.author);
 
+    const id = req.body._id;
     const title = req.body.title;
     const author = req.body.author;
 
@@ -30,6 +31,7 @@ app.post('/msg',urlencodedParser , function(req, res) {
     // });
 
     const book = new BookModel({
+        id,
         title,
         author
     });
@@ -47,22 +49,46 @@ app.post('/msg',urlencodedParser , function(req, res) {
     res.redirect('/');
 });
 
-app.post('/delete', urlencodedParser, function(req,res){
-    const idx = req.body.index; 
-    console.log("Delete book " + idx);
-    books.splice(idx, 1); //usuwanie elementu z tablicy
-    
+app.post('/delete', urlencodedParser, async function(req,res){
+    // const idx = req.body.index; 
+    // console.log("Delete book " + idx);
+    // books.splice(idx, 1); //usuwanie elementu z tablicy
+
+    const id = req.body._id;
+
+    await BookModel.deleteOne({_id : id});
+    // await BookModel.findByIdAndDelete
+
     res.redirect('/');
 });
 
-app.post('/change', urlencodedParser, function(req,res){
+app.get('/update/:id', async function(req, res) {
+    const book = await BookModel.findById(req.params.id);
+
+    res.render('update', {book})
+});
+
+app.post('/update', urlencodedParser, async function(req,res){
+    const id = req.body._id;
+
+    // const document = BookModel.find({_id : id});
+    // document.title = req.body.title;
+    // document.author = req.body.title;
+    // await document.save();
+
+    await BookModel.updateOne({_id : id}, 
+        {
+            title : req.body.title, 
+            author : req.body.author
+        });
+        
+    res.redirect('/');
     
 });
 
-// ----- LAB 3-4 ------
-
 // 1. POLACZENIE Z BAZA
 var mongoose = require('mongoose');
+const { title } = require('process');
 var mongoDB = 'mongodb://127.0.0.1/books_db';
 mongoose.connect(mongoDB);
 var db = mongoose.connection;
